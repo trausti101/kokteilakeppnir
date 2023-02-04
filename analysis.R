249ae06dcd19c4d63de5b20beda31c9f9ca48709
trausti101 <trausti101@gmail.com>
2023-02-04 16:23:15 +0000

    Tidy data
    -Dropped NA rows
    -Changed "all" from vector to list
    -Renamed list items to reflect drink

--- analysis.R
+++ analysis.R
@@ -1,8 +1,18 @@
-Agla <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Agla")
-Trausti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Trausti")
-Hjalti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Hjalti")
-Jónas <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Jónas")
-Harpa <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Harpa")
-Kata <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Kata")
+Agla <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Agla") %>%
+  drop_na()
+Trausti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Trausti") %>%
+  drop_na()
+Hjalti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Hjalti") %>%
+  drop_na()
+Jónas <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Jónas") %>%
+  drop_na()
+Harpa <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Harpa") %>%
+  drop_na()
+Kata <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Kata") %>%
+  drop_na()
 
-all <- c(Agla, Trausti, Hjalti, Jónas, Harpa, Kata)
+all <- list(Agla, Trausti, Hjalti, Jónas, Harpa, Kata)
+
+names(all) <- c("Agla", "Trausti", "Hjalti", "Jónas", "Harpa", "Kata")
+
+
