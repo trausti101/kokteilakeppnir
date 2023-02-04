821842a5753acadf82968debf651dbc9d97b73f7
trausti101 <trausti101@gmail.com>
2023-02-04 16:10:30 +0000

    Reading
    -Added all sheets
    -Created master list

---
+++ analysis.R
@@ -0,0 +1,8 @@
+Agla <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Agla")
+Trausti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Trausti")
+Hjalti <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Hjalti")
+Jónas <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Jónas")
+Harpa <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Harpa")
+Kata <- read_sheet("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", sheet = "Kata")
+
+all <- c(Agla, Trausti, Hjalti, Jónas, Harpa, Kata)
