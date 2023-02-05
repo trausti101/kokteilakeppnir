4d0f8754d92a1f9ce26e751f5455f80e5d2847cd
trausti101 <trausti101@gmail.com>
2023-02-05 23:34:54 +0000

    Merging of commits

--- analysis.R
+++ analysis.R
@@ -18,6 +18,8 @@ all <- list(Agla, Trausti, Hjalti, Jónas, Harpa, Kata)
 
 names(all) <- c("Agla", "Trausti", "Hjalti", "Jónas", "Harpa", "Kata")
 
+allSingle <- bind_rows(all, .id = "id")
+
 ggplot(data = filter(gather(all$Agla), key != "Dómari"))+
   geom_density(aes(x = as.numeric(value), fill = key), alpha = 0.4)+
   scale_x_continuous(limits = c(0,5))
