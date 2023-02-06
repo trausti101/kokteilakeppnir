6b57674e54e3f89122d5c9e9716ad4e338902726
trausti101 <trausti101@gmail.com>
2023-02-06 00:14:50 +0000

    Faceted plot
    -Faceted plot finally works
    -Added allSingle tibble
    	-Pivoted long version of all
    	-Removed "Dómari" Row

--- analysis.R
+++ analysis.R
@@ -7,20 +7,28 @@ loadCocktail <- function(worksheet, participant) {
 }
 
 Agla <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Agla")
-Trausti <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Trausti")
-Hjalti <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Hjalti")
-Jónas <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Jónas")
-Harpa <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Harpa")
-Kata <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Kata")
+Trausti <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Trausti") 
+Hjalti <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Hjalti") 
+Jónas <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Jónas") 
+Harpa <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Harpa") 
+Kata <- loadCocktail("1W29bIe_DUZgTCk9h8yACCU380T6xmR7NblG0IQF-tH8", "Kata") 
 
 
 all <- list(Agla, Trausti, Hjalti, Jónas, Harpa, Kata)
 
 names(all) <- c("Agla", "Trausti", "Hjalti", "Jónas", "Harpa", "Kata")
 
-allSingle <- bind_rows(all, .id = "id")
+allSingle <- all %>%
+  mutate(Dómari = NULL) %>%
+  bind_rows(.id = "id") %>%
+  pivot_longer(where(is.numeric)) %>%
+  group_by(id, name) %>%
+  arrange(.by_group = TRUE)
 
-ggplot(data = filter(gather(all$Agla), key != "Dómari"))+
-  geom_density(aes(x = as.numeric(value), fill = key), alpha = 0.4)+
-  scale_x_continuous(limits = c(0,5))
+ggplot(data = allSingle)+
+  geom_density(aes(x = as.numeric(value), fill = name), alpha = 0.4)+
+  scale_x_continuous(limits = c(0,5))+
+  labs(x = "Stig", fill = "Flokkur")+
+  facet_wrap(~id)
+  
   
